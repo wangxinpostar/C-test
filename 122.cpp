64e4183e19e8987c6eb8114ea904e67e1cd35815
wangx <1124434059@qq.com>
2023-01-23 13:13:17 +0800

    1月22日

---
+++ 122.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+void f(int n)
+{
+    int x=1;
+    while ((1<<x)<=n)
+    {
+        x++;
+    }
+    x--;
+    if(x==0)cout<<"2(0)";
+    if(x==1)cout<<"2";
+    if(x>1)
+    {
+        cout<<"2(";
+        f(x);
+        cout<<")";
+    }
+    if((1<<x)!=n)
+    {
+        cout<<"+";
+        f(n-(1<<x));
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    f(n);
+    //
+}
