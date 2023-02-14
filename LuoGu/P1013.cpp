fdf9ffb271f8deee8b3fb6eb202c477c99b8f880
wangx <1124434059@qq.com>
2023-02-14 14:23:01 +0800

    ok

---
+++ LuoGu/P1013.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<algorithm>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<set>
+#include<map>
+using namespace std;
+int main()
+{
+    
+}
