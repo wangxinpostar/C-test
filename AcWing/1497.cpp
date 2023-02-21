a449b78adcf584af9676f383abda7e33e1f1f9f5
wangx <1124434059@qq.com>
2023-02-22 01:22:28 +0800

    2/22

--- AcWing/1497.cpp
+++ AcWing/1497.cpp
@@ -9,6 +9,16 @@
 using namespace std;
 int hou[100],zhong[100],id[100];
 int n;
+vector<int>ans[100];
+void build(int fl,int fr,int sl,int sr,int n)
+{
+    if(fl>fr)return;
+    int val=hou[fr];
+    ans[n].push_back(val);
+    int k=id[val];
+    build(fl,k-sl-1+fl,sl,k-1,n+1);
+    build(k-sl+fl,fr-1,k+1,sr,n+1);
+}
 int main()
 {
     cin>>n;
@@ -21,5 +31,12 @@ int main()
         cin>>zhong[i];
         id[zhong[i]]=i;
     }
-    
+    build(1,n,1,n,0);
+    for(int i=0;i<n;i++)
+    {
+        for(auto j:ans[i])
+        {
+            cout<<j<<" ";
+        }
+    }
 }
